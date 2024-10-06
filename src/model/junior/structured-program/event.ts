14af82f386a5df3f5455f39790defd29bdcdaa07
Ben North <ben.north@tcd.ie>
2024-10-06 20:40:33 +0100

    EventHandlerOps.clone(): Define

--- src/model/junior/structured-program/event.ts
+++ src/model/junior/structured-program/event.ts
@@ -132,4 +132,13 @@ export class EventHandlerOps {
     const codeHash = await hexSHA256(handler.pythonCode);
     return `${eventFingerprint}:${codeHash}`;
   }
+
+  /** Return a deep clone of the given `handler`, except that the clone
+   * has a fresh `id`. */
+  static clone(handler: EventHandler): EventHandler {
+    const id = UuidOps.newRandom();
+    const event = EventDescriptorOps.clone(handler.event);
+    const pythonCode = handler.pythonCode;
+    return { id, event, pythonCode };
+  }
 }
