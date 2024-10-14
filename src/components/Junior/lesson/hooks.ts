9e15def54031c82db24b38c38711e18459c79497
Ben North <ben.north@tcd.ie>
2024-10-14 14:55:36 +0100

    useMappedLinkedSpecimen(): Define hook

--- src/components/Junior/lesson/hooks.ts
+++ src/components/Junior/lesson/hooks.ts
@@ -1,6 +1,9 @@
 import { useStoreState } from "../../../store";
 import { LinkedJrTutorial } from "../../../model/junior/jr-tutorial";
-import { LinkedContentKind } from "../../../model/linked-content";
+import {
+  LinkedContentKind,
+  LinkedSpecimen,
+} from "../../../model/linked-content";
 
 const useHasLinkedContentOfKind = (tgtKind: LinkedContentKind): boolean =>
   useStoreState((state) => {
@@ -32,6 +35,23 @@ export function useMappedLinkedJrTutorial<Result>(
 
     if (contentState.content.kind !== "jr-tutorial")
       throw new Error("linked lesson is not suitable");
+
+    return mapContent(contentState.content);
+  }, eqResult);
+}
+
+export function useMappedLinkedSpecimen<Result>(
+  mapContent: (specimen: LinkedSpecimen) => Result,
+  eqResult?: (prev: Result, next: Result) => boolean
+) {
+  return useStoreState((state) => {
+    const contentState = state.activeProject.linkedContentLoadingState;
+
+    if (contentState.kind !== "succeeded")
+      throw new Error("linked content has not been loaded");
+
+    if (contentState.content.kind !== "specimen")
+      throw new Error("linked content is wrong kind");
 
     return mapContent(contentState.content);
   }, eqResult);
