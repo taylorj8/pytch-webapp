3b14ca92fd94ffb2f482791141f3b50f373f2ac3
Ben North <ben.north@tcd.ie>
2024-10-14 14:58:04 +0100

    SpecimenInformation: Define component

---
+++ src/components/Junior/lesson/SpecimenInformation.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { EmptyProps } from "../../../utils";
+import { useLinkedSpecimen } from "./hooks";
+import { WidthMonitor } from "../WidthMonitor";
+
+export const SpecimenInformation: React.FC<EmptyProps> = () => {
+  const specimen = useLinkedSpecimen();
+  const specimenName = specimen.lesson.project.name;
+
+  return (
+    <div className="Junior-LessonContent-container">
+      <WidthMonitor nonStageWd={980} />
+      <div className="Junior-LessonContent-HeaderBar">
+        <div className="specimen-name">{specimenName}</div>
+      </div>
+
+      <div className="Junior-LessonContent-inner-container">
+        <div className="Junior-LessonContent abs-0000-oflow">
+          <div className="content">
+            This project is based on the code for <em>{specimenName}</em>.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
