dc088b498f9327004c7b727ea89bb997eb6a9d8d
Ben North <ben.north@tcd.ie>
2024-10-25 13:58:48 +0100

    aceRowSelector(): Define helper

--- src/components/Junior/PytchScriptEditor-scroller.ts
+++ src/components/Junior/PytchScriptEditor-scroller.ts
@@ -2,3 +2,6 @@ function isNull<T>(x: T | null, xName: string): x is null {
   if (x == null) console.log(`${xName} is null`);
   return x == null;
 }
+
+const aceRowSelector = (row1b: number) =>
+  `.ace_text-layer .ace_line:nth-child(${row1b})`;
