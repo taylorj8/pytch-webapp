c2f73efc1934100472ad23e1e4eb82a9c110675d
Ben North <ben.north@tcd.ie>
2024-10-25 13:59:48 +0100

    hasLiveCursor(): Define helper

--- src/components/Junior/PytchScriptEditor-scroller.ts
+++ src/components/Junior/PytchScriptEditor-scroller.ts
@@ -5,3 +5,13 @@ function isNull<T>(x: T | null, xName: string): x is null {
 
 const aceRowSelector = (row1b: number) =>
   `.ace_text-layer .ace_line:nth-child(${row1b})`;
+
+function hasLiveCursor(parentDiv: HTMLElement): boolean {
+  // scrollCursorRowIntoView() can get called when the editor does not
+  // have the focus, e.g., when expanding or collapsing the info panel.
+  const maybeLiveCursors = parentDiv.querySelector(
+    ".ace_cursor-layer:not(.ace_hidden-cursors)"
+  );
+  // This is expected so don't log() if null:
+  return maybeLiveCursors != null;
+}
