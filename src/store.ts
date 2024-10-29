d4b12dcdf4e70e666e90f58e3db96252747de877
Ben North <ben.north@tcd.ie>
2024-10-29 12:46:34 +0000

    Provide easy-peasy store via PYTCH_CYPRESS

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { createStore, createTypedHooks } from "easy-peasy";
 import { pytchAppModel, IPytchAppModel } from "./model";
+import { PYTCH_CYPRESS } from "./utils";
 
 const { useStoreActions, useStoreState, useStoreDispatch } =
   createTypedHooks<IPytchAppModel>();
@@ -8,5 +9,7 @@ export { useStoreActions, useStoreDispatch, useStoreState };
 
 const store = createStore(pytchAppModel);
 export type PytchAppStore = typeof store;
+
+PYTCH_CYPRESS()["easyPeasyStore"] = store;
 
 export default store;
