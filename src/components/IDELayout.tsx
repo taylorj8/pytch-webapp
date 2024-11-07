922a36653a55a9a1fba1460e556f390d74c75a5a
Ben North <ben.north@tcd.ie>
2024-11-07 11:23:51 +0000

    IDELayout: Define component
    Mostly copied from existing Junior/IDEContents component.  Modals and
    fullscreen to follow.

---
+++ src/components/IDELayout.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import classNames from "classnames";
+import { useStoreActions, useStoreState } from "../store";
+import { useJrEditState } from "./Junior/hooks";
+import { EmptyProps } from "../utils";
+import { DivSettingWindowTitle } from "./DivSettingWindowTitle";
+import { ActivityBar } from "./Junior/ActivityBar";
+import { ActivityContent } from "./Junior/ActivityContent";
+import { EditorAndOutErr } from "./EditorAndOutErr";
+import { StageAndActorsOrAssets } from "./StageAndActorsOrAssets";
+
+export const IDELayout: React.FC<EmptyProps> = () => {
+  const projectId = useStoreState((state) => state.activeProject.project.id);
+  const projectName = useStoreState(
+    (state) => state.activeProject.project.name
+  );
+  const activityContentFullStateLabel = useJrEditState(
+    (s) => s.activityContentFullStateLabel
+  );
+  const maybeConnectToLiveReloadServer = useStoreActions(
+    (actions) => actions.reloadServer.maybeConnect
+  );
+
+  useEffect(() => maybeConnectToLiveReloadServer());
+
+  const classes = classNames(
+    "IDELayout",
+    "abs-0000",
+    `activity-content-${activityContentFullStateLabel}`
+  );
+
+  return (
+    <DivSettingWindowTitle
+      className={classes}
+      windowTitle={`Pytch: ${projectName}`}
+      data-project-id={projectId}
+    >
+      <ActivityBar />
+      <ActivityContent />
+      <EditorAndOutErr />
+      <StageAndActorsOrAssets />
+    </DivSettingWindowTitle>
+  );
+};
