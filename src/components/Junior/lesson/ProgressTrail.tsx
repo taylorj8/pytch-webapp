dc1444c83714e6704569d277d301dde0a1db6e7a
Ben North <ben.north@tcd.ie>
2024-11-10 18:00:18 +0000

    GenericProgressTrail: Define component
    And props type.  Extract the existing logic from ProgressTrail,
    simplifying slightly here and there, and using props for any values or
    calculations.

--- src/components/Junior/lesson/ProgressTrail.tsx
+++ src/components/Junior/lesson/ProgressTrail.tsx
@@ -23,6 +23,80 @@ const ProgressTrailNode: React.FC<ProgressTrailNodeProps> = ({ kind }) => {
   return <div className={nodeClasses}>{objContent}</div>;
 };
 
+type GenericProgressTrailProps = {
+  nProgressStages: number;
+  activeChapterIndex: number;
+  setChapterIndex(idx: number): void;
+  nodeKindFromIndex(idx: number): ProgressNodeKind;
+  cloneChapterTitleElt(idx: number): HTMLElement;
+  canJumpHereFromIndex(idx: number): boolean;
+};
+const GenericProgressTrail: React.FC<GenericProgressTrailProps> = ({
+  nProgressStages,
+  activeChapterIndex,
+  setChapterIndex,
+  nodeKindFromIndex,
+  cloneChapterTitleElt,
+  canJumpHereFromIndex,
+}) => {
+  const chapterTitleElt = cloneChapterTitleElt(activeChapterIndex);
+
+  const nodeDivs = range(nProgressStages).map((idx) => (
+    <ProgressTrailNode key={idx} kind={nodeKindFromIndex(idx)} />
+  ));
+
+  const maybeChapterNumberLabel = activeChapterIndex > 0 && (
+    <span className="chapter-number">{activeChapterIndex} —</span>
+  );
+
+  const nodeBackgrounds = range(nProgressStages).map((idx) => {
+    const isActive = idx === activeChapterIndex;
+    const classes = classNames("progress-node-background", { isActive });
+    return <div key={idx} className={classes} />;
+  });
+
+  const nodeHoverTargets = range(nProgressStages).map((idx) => {
+    const canJumpHere = canJumpHereFromIndex(idx);
+    const contentElt = cloneChapterTitleElt(idx);
+
+    const tooltip = (
+      <div className="progress-node-tooltip">
+        {!canJumpHere && <FontAwesomeIcon className="locked" icon="lock" />}
+        <RawElement element={contentElt} />
+      </div>
+    );
+
+    const onClick = canJumpHere ? () => setChapterIndex(idx) : () => void 0;
+    const classes = classNames("progress-node-hover-target", { canJumpHere });
+
+    return (
+      <div
+        key={idx}
+        data-chapter-index={`${idx}`}
+        className={classes}
+        onClick={onClick}
+      >
+        {tooltip}
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className="ProgressTrail">
+        <div className="node-backgrounds">{nodeBackgrounds}</div>
+        <div className="track" />
+        <div className="nodes">{nodeDivs}</div>
+        <div className="node-hover-targets">{nodeHoverTargets}</div>
+      </div>
+      <div className="chapter-title">
+        {maybeChapterNumberLabel}
+        {chapterTitleElt.innerText}
+      </div>
+    </>
+  );
+};
+
 export const ProgressTrail: React.FC<EmptyProps> = () => {
   const linkedTutorial = useLinkedJrTutorial();
   const tutorialContent = linkedTutorial.content;
