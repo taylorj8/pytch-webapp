8564781a541274d0c6f79d937611699ebe782d4d
Ben North <ben.north@tcd.ie>
2024-11-11 10:14:53 +0000

    StageControls: Always include full-screen button
    For "flat" programs, the full-screen button used to be part of the
    layout switcher, but that will be gone soon.  So always include the
    button in the StageControls.

--- src/components/StageControls.tsx
+++ src/components/StageControls.tsx
@@ -137,8 +137,6 @@ export const StageControls: React.FC<EmptyProps> = () => {
     (actions) => actions.ideLayout.setIsFullScreen
   );
 
-  const programKind = project.program.kind;
-
   const handleSave = () => requestSyncToStorage();
 
   const runDisplayScreenshot = useRunFlow((f) => f.displayScreenshotFlow);
@@ -157,7 +155,7 @@ export const StageControls: React.FC<EmptyProps> = () => {
   const copyArgs = { sourceProjectId: project.id, sourceName: project.name };
   const onCreateCopy = () => runSaveProjectAs(copyArgs);
 
-  const mFullScreenButton = programKind === "per-method" && (
+  const fullScreenButton = (
     <Button className="full-screen" onClick={() => setIsFullScreen(true)}>
       <FontAwesomeIcon className="fa-lg" icon="expand" />
     </Button>
@@ -187,7 +185,7 @@ export const StageControls: React.FC<EmptyProps> = () => {
       >
         <span>Save</span>
       </Button>
-      {mFullScreenButton}
+      {fullScreenButton}
       <Link to="/">
         <Button>
           <FontAwesomeIcon aria-label="Home" icon="home" />
