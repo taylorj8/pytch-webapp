340aef7dcd4ac2a3adda502ab1af978411a683f2
Ben North <ben.north@tcd.ie>
2024-11-12 14:44:43 +0000

    Modals: Define local component

--- src/components/IDELayout.tsx
+++ src/components/IDELayout.tsx
@@ -2,13 +2,28 @@ import React, { useEffect } from "react";
 import classNames from "classnames";
 import { useStoreActions, useStoreState } from "../store";
 import { useJrEditState } from "./Junior/hooks";
-import { EmptyProps } from "../utils";
+import { assertNever, EmptyProps } from "../utils";
 import { DivSettingWindowTitle } from "./DivSettingWindowTitle";
 import { ActivityBar } from "./Junior/ActivityBar";
 import { ActivityContent } from "./Junior/ActivityContent";
 import { EditorAndOutErr } from "./EditorAndOutErr";
 import { StageAndActorsOrAssets } from "./StageAndActorsOrAssets";
 import { FullScreenLayout } from "./FullScreenLayout";
+import { Modals as PerMethodModals } from "./Junior/Modals";
+
+const Modals: React.FC<EmptyProps> = () => {
+  const programKind = useStoreState(
+    (state) => state.activeProject.project.program.kind
+  );
+  switch (programKind) {
+    case "flat":
+      return false;
+    case "per-method":
+      return <PerMethodModals />;
+    default:
+      return assertNever(programKind);
+  }
+};
 
 export const IDELayout: React.FC<EmptyProps> = () => {
   const projectId = useStoreState((state) => state.activeProject.project.id);
