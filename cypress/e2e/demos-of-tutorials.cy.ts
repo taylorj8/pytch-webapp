0357647dab10912582acc986db543102e678d19a
Ben North <ben.north@tcd.ie>
2025-01-09 11:43:38 +0000

    Add e2e test that all tutorial demos work

---
+++ cypress/e2e/demos-of-tutorials.cy.ts
@@ -0,0 +1,27 @@
+context("Demos of all tutorials", () => {
+  const kNumberOfTutorials = 18;
+
+  function assertNTutorials() {
+    cy.get("ul.tutorial-list li").should("have.length", kNumberOfTutorials);
+  }
+
+  function launchNthTutorialDemo(tutorialIndex: number) {
+    const childNumber = tutorialIndex + 1;
+    cy.get(
+      `ul.tutorial-list li:nth-child(${childNumber})` +
+        ' button[title="Try this project"]'
+    ).click();
+  }
+
+  it("can run demos", () => {
+    cy.pytchResetDatabase({ initialUrl: "/tutorials/" });
+    assertNTutorials();
+
+    for (let tutIdx = 0; tutIdx != kNumberOfTutorials; ++tutIdx) {
+      launchNthTutorialDemo(tutIdx);
+      cy.contains("Click the green flag to run");
+      cy.pytchHomeFromIDE();
+      cy.get(".NavBar li").contains("Tutorials").click();
+    }
+  });
+});
