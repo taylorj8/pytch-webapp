a1dbc58e2d3817357a7e523e720b976fad59c81d
Ben North <ben.north@tcd.ie>
2025-01-16 12:26:34 +0000

    ActionButtons: Define local component

--- src/components/Junior/lesson/SpecimenInformation.tsx
+++ src/components/Junior/lesson/SpecimenInformation.tsx
@@ -1,7 +1,36 @@
 import React from "react";
-import { EmptyProps } from "../../../utils";
+import { assertNever, EmptyProps } from "../../../utils";
 import { useLinkedSpecimen } from "./hooks";
 import { WidthMonitor } from "../WidthMonitor";
+import { Button } from "react-bootstrap";
+import { LinkedSpecimen } from "../../../model/linked-content";
+import { useStoreState } from "../../../store";
+import { codeTextEnsuringFlat } from "../../hooks/code-text";
+import { useRunFlow } from "../../../model";
+
+type ActionButtonsProps = { specimen: LinkedSpecimen };
+const ActionButtons: React.FC<ActionButtonsProps> = ({ specimen }) => {
+  const runFlow = useRunFlow((f) => f.viewCodeDiffFlow);
+  const program = useStoreState((state) => state.activeProject.project.program);
+  const specimenKind = specimen.lesson.project.program.kind;
+  switch (specimenKind) {
+    case "flat": {
+      const originalCodeText = specimen.lesson.project.program.text;
+      const currentCodeText = codeTextEnsuringFlat("<ActionButtons>", program);
+      const launch = () => {
+        runFlow({
+          textA: originalCodeText,
+          textB: currentCodeText,
+        });
+      };
+      return <Button onClick={launch}>Compare to original</Button>;
+    }
+    case "per-method":
+      return false;
+    default:
+      return assertNever(specimenKind);
+  }
+};
 
 export const SpecimenInformation: React.FC<EmptyProps> = () => {
   const specimen = useLinkedSpecimen();
