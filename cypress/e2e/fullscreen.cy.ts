0abc86c855235a3e7de581f3c885c317d064646b
Ben North <ben.north@tcd.ie>
2025-01-21 14:38:03 +0000

    Remove stale e2e tests
    Tests
    
        "does not resize stage in IDE layout"
        "remembers IDE-layout stage dimensions"
    
    no longer relevant.

--- cypress/e2e/fullscreen.cy.ts
+++ cypress/e2e/fullscreen.cy.ts
@@ -1,21 +1,10 @@
 /// <reference types="cypress" />
 
-import {
-  stageWidth,
-  stageHeight,
-  stageFullScreenBorderPx,
-} from "../../src/constants";
+import { stageFullScreenBorderPx } from "../../src/constants";
 
 context("Full-screen layout", () => {
   before(() => {
     cy.pytchExactlyOneProject();
-  });
-
-  it("does not resize stage in IDE layout", () => {
-    cy.get(".LayoutChooser button.wide-info").click().click();
-    cy.viewport(800, 600);
-    cy.get("#pytch-canvas").its("0.width").should("eq", stageWidth);
-    cy.get("#pytch-canvas").its("0.height").should("eq", stageHeight);
   });
 
   it("enters and leaves full-screen layout", () => {
@@ -28,31 +17,6 @@ context("Full-screen layout", () => {
     cy.get(".CodeEditor");
     cy.get(".InfoPanel");
     cy.get(".LayoutChooser");
-  });
-
-  it("remembers IDE-layout stage dimensions", () => {
-    // Click twice to ensure default stage size to start:
-    cy.get(".LayoutChooser button.wide-info").click().click();
-    cy.pytchDragStageDivider(50);
-    cy.get("#pytch-canvas")
-      .its("0.width")
-      .then((width) => {
-        cy.get("#pytch-canvas")
-          .its("0.height")
-          .then((height) => {
-            cy.get(".LayoutChooser .full-screen").click();
-            cy.get(".CodeEditor").should("not.exist");
-
-            cy.get("#pytch-canvas").its("0.width").should("be.gt", width);
-            cy.get("#pytch-canvas").its("0.height").should("be.gt", height);
-
-            cy.get(".leave-full-screen").click();
-            cy.get(".CodeEditor");
-
-            cy.get("#pytch-canvas").its("0.width").should("eq", width);
-            cy.get("#pytch-canvas").its("0.height").should("eq", height);
-          });
-      });
   });
 
   [
