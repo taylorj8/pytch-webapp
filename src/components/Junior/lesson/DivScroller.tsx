05918c3a185411d7c16e947d4572b745bb792139
Ben North <ben.north@tcd.ie>
2025-01-28 15:21:05 +0000

    Add docstring

--- src/components/Junior/lesson/DivScroller.tsx
+++ src/components/Junior/lesson/DivScroller.tsx
@@ -6,6 +6,13 @@ export type DivScrollerProps = {
   containerDivRef: React.RefObject<HTMLDivElement>;
 };
 
+/** Remember the scroll position of each individual "page".  (In both
+ * current use-cases, a "page" is a chapter either in a flat-tutorial or
+ * in a per-method-lesson.)  Restore the scroll position when displaying
+ * that "page", perhaps after the user checks something in a previous
+ * chapter or in the help.  The map is stored in a global variable
+ * outside the Easy-Peasy state; this seems to be working OK.  The map
+ * is cleared when booting the IDE for a different project. */
 export const DivScroller: React.FC<DivScrollerProps> = (props) => {
   // There is a flicker which is not removed by using useLayoutEffect(),
   // perhaps because the flicker is in a different component.  Accept it
