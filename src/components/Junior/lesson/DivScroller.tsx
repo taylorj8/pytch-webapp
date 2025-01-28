f095e3dd078b03f7d7e99ce7248123d79236cc83
Ben North <ben.north@tcd.ie>
2025-01-28 14:54:00 +0000

    DivScroller: Redesign to use new pageKey prop

--- src/components/Junior/lesson/DivScroller.tsx
+++ src/components/Junior/lesson/DivScroller.tsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { useMappedLinkedJrTutorial } from "./hooks";
-import { useJrEditState } from "../hooks";
+import React, { useEffect } from "react";
+import { scrollTopFromPageKey } from "../../../model/junior/jr-tutorial";
 
 export type DivScrollerProps = {
+  pageKey: number;
   containerDivRef: React.RefObject<HTMLDivElement>;
 };
 
 // TODO: Compare this and the equivalent for "flat" tutorials and settle
 // on one approach.
 export const DivScroller: React.FC<DivScrollerProps> = (props) => {
-  const chapterIndex = useMappedLinkedJrTutorial(
-    (tutorial) => tutorial.interactionState.chapterIndex
-  );
-  const tutorialChapterScrollTop = useJrEditState(
-    (s) => s.tutorialChapterScrollTop
-  );
-
-  const [lastScrolledChapIdx, setLastScrolledChapIdx] = useState(0);
-
   useEffect(() => {
     const containerDiv = props.containerDivRef.current;
     if (containerDiv != null) {
-      if (lastScrolledChapIdx !== chapterIndex) {
-        containerDiv.scrollTop = 0;
-        setLastScrolledChapIdx(chapterIndex);
-      } else {
-        containerDiv.scrollTop = tutorialChapterScrollTop;
-      }
+      const desiredScrollTop = scrollTopFromPageKey.get(props.pageKey) ?? 0;
+      setImmediate(() => {
+        containerDiv.scrollTop = desiredScrollTop;
+      });
+
+      const updateScrollTopMap = () =>
+        scrollTopFromPageKey.set(props.pageKey, containerDiv.scrollTop);
+
+      containerDiv.addEventListener("scroll", updateScrollTopMap);
+
+      return () =>
+        containerDiv.removeEventListener("scroll", updateScrollTopMap);
     }
   });
 
